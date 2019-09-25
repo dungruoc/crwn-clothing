4eec24e30eb392e12abb7e332fc50076d3ce28e8
dungruoc <minhdung.dang@gmail.com>
2019-09-25 14:41:35 +0700

    new shop page, new collection-preview and collection-item components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
-
-const HatsPage = () => (
-  <h1>Hats</h1>
-)
 
 function App() {
   return (
     <div>
-      <Route exact="true" path="/" component={HomePage} />
-      <Route exact="true" path="/shop/hats" component={HatsPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
